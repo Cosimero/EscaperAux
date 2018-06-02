48bda589c7364623856625dddc16cbda034cb167
Cosimero <ipvalders@me.com>
2018-06-02 12:46:38 +0200

    Modifications

--- EscaperAux/src/Map.java
+++ EscaperAux/src/Map.java
@@ -2,9 +2,19 @@ import java.util.ArrayList;
 import java.util.List;
 public class Map {
 	private String name_;
-	private List<Room> map_ = new ArrayList<>();
+	private List<Room> rooms_ = new ArrayList<>();
 	
-	public Map() {
-		
+	public Map(String name) {
+		name_=name;
 	}
+
+	public List<Room> getRooms_() {
+		return rooms_;
+	}
+
+	public void setRooms_(List<Room> rooms_) {
+		this.rooms_ = rooms_;
+	}
+	
+	
 }
--- EscaperAux/src/Room.java
+++ EscaperAux/src/Room.java
@@ -1,5 +1,6 @@
 import java.util.*;
 public class Room {
+	private String name;
 	private Coordinates coordinates_;
 	private List<Corridor> connectedBy_ = new ArrayList<>(); 
 	
