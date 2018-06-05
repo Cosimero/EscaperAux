0ccde497b2fe999ba248fa5da141e04225132b3a
Denis <drobny@zoho.com>
2018-06-05 23:59:51 +0200

    Parser constructor fixed

--- EscaperAux/src/Main.java
+++ EscaperAux/src/Main.java
@@ -5,12 +5,11 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		
-		Parser parser = new Parser("database2.txt","database2.txt"); // TODO load from GUI
+		Parser parser = new Parser("database2.txt","database3.txt"); // TODO load from GUI
 		Map map = parser.getMap();
 		// Draw it out and wait for users' selection then provide computed route
 		MapDrawer drawer = new MapDrawer(map);
 		drawer.drawMap();
-
 	}
 
 }
